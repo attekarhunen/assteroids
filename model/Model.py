e902f13b6de616cd1c06e08c757c10e38e61a61b
atte <Atte Karhunen>
2018-12-29 09:39:21 +0200

    asteroids are spawned in random positions again

--- model/Model.py
+++ model/Model.py
@@ -1,9 +1,10 @@
 from asteroid import Asteroid
+import random
 
 
 class Model(object):
-    DISPLAY_W = 1200
-    DISPLAY_H = 800
+    DISPLAY_W = 800
+    DISPLAY_H = 600
     BLACK = (0, 0, 0)
     WHITE = (255, 255, 255)
     RED = (255, 0, 0)
@@ -20,4 +21,4 @@ class Model(object):
         print("Model initialized")
 
         for i in range(0, 5):
-            self.asteroids.append(Asteroid.Asteroid((50, 50)))
+            self.asteroids.append(Asteroid.Asteroid(((random.uniform(100, self.DISPLAY_W - 100)), (random.uniform(100, self.DISPLAY_H - 100)))))
